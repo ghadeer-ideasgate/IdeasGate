12638f2bcdca23e3d26e7cd374aa2f8b7ac72c79
Ghadeer <ghadeer.ideasgate@gmail.com>
2019-10-08 16:56:01 +0200

    add clients,portfolio basic html

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,3 +6,52 @@
 // https://getbootstrap.com/docs/4.0/components/popovers/#example-enable-popovers-everywhere
 // $(function () { $('[data-toggle="popover"]').popover(); });
 
+// portfolio js code
+
+/*-------------------------------------
+     Protfolio
+    -------------------------------------*/
+
+
+     //Protfolio popup
+
+     $('.portfolio-gallery-set').magnificPopup({
+      type: 'image',
+         gallery:{
+     enabled:true
+     }
+
+     });
+
+     //protfolio filter set
+     $('.portfolio-filter li').on('click', function(event) {
+         $(this).siblings('.active').removeClass('active');
+         $(this).addClass('active');
+         event.preventDefault();
+     });
+
+     //protfolio isotope
+     var $varPortfolioMasonry = $('.portfolio-masonry');
+     if (typeof imagesLoaded == 'function') {
+         imagesLoaded($varPortfolioMasonry, function() {
+             setTimeout(function() {
+                 $varPortfolioMasonry.isotope({
+                     itemSelector: '.portfolio-item',
+                     resizesContainer: false,
+                     layoutMode: 'masonry',
+                     filter: '*'
+                 });
+             }, 500);
+
+         });
+     };
+
+    //protfolio filtering
+    $('.portfolio-filter').on('click', 'a', function() {
+         $(this).addClass('current');
+         var filterValue = $(this).attr('data-filter');
+         $(this).parents('.portfolio-filter-wrap').next().isotope({
+             filter: filterValue
+         });
+     });
+
