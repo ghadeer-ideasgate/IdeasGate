2a9a785cdf4c35959ca783950f04c7f1bffe5ea6
Ghadeer <ghadeer.ideasgate@gmail.com>
2019-10-14 15:52:15 +0200

    without slider ..clients and why us are done

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -48,26 +48,8 @@
   });
 })(jQuery, window, document);
 
-$(document).ready(function(){
-  $('.customer-logos').slick({
-      slidesToShow: 6,
-      slidesToScroll: 1,
-      autoplay: true,
-      autoplaySpeed: 1500,
-      arrows: false,
-      dots: false,
-      pauseOnHover: false,
-      responsive: [{
-          breakpoint: 768,
-          settings: {
-              slidesToShow: 4
-          }
-      }, {
-          breakpoint: 520,
-          settings: {
-              slidesToShow: 3
-          }
-      }]
-  });
-});
 
+
+$('#clientsCarousel').carousel({
+  interval: 5000
+});
